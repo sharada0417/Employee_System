4396141c327b6ad1a866be43f395fb05b3d64c67
sharada417 <sharada.kanchana2001@gmail.com>
2025-04-08 23:20:23 +0530

    Update ResponseDTO

--- Backend/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Backend/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,11 +1,34 @@
 package com.example.demo.controller;
-import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Service;
+import com.example.demo.dto.EmployeeDTO;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Service
-@Transactional
+@RestController
+@RequestMapping("api/v1/employee")
 public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @PostMapping(value = "/saveEmployee")
+    public ResponseEntity saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        try {
+            String res = employeeService.saveEmployee(employeeDTO);
+            if(res.equals("08")){
+
+            } else if(res.equals("06")){
+
+            } else {
+
+            }
+        } catch (Exception ex) {
+
+        }
+    }
 
 
 }
