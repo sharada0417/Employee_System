24219783e9b089eb3b52cf530ba9993b8e3cbe3b
sharada417 <sharada.kanchana2001@gmail.com>
2025-04-10 02:28:31 +0530

    Implement Redux tool kit

---
+++ Frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './features/counterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
